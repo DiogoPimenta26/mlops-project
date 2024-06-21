cf0a1204aed1466123a4e4b3838e1a22be23158c
beatrizdsantosss <20230521@novaims.unl.pt>
2024-06-21 23:05:17 +0100

    Added train_test_split pipeline and updated pipeline registry

--- src/mlops_project/pipelines/train_test_split/nodes.py
+++ src/mlops_project/pipelines/train_test_split/nodes.py
@@ -5,7 +5,7 @@ generated using Kedro 0.19.5
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def split_data(data: pd.DataFrame, test_size: float, random_state: int):
+def split_data(data: pd.DataFrame, test_size: float, random_state: int) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame):
     X = data.drop(columns=['target'])
     y = data['target']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
